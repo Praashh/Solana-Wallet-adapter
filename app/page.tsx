6d9fa0ed6740b91aa5ae18644670207d783ced3c
Prashant Varma <prashantvarma5083@gmail.com>
2024-04-14 22:32:34 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+"use client"
 import Image from "next/image";
+import Link from "next/link";
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function Home() {
+
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+           started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
@@ -16,28 +21,24 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+           <svg width="50" height="50" viewBox="0 0 123 125" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M32.2017 101.645C30.8339 100.233 31.001 98.7166 32.7032 97.096L110.748 21.611C112.45 19.9904 113.968 19.8779 115.3 21.2736L118.836 24.923C120.204 26.3351 120.037 27.8515 118.335 29.4721L40.29 104.957C38.5878 106.578 37.0705 106.69 35.7382 105.295L32.2017 101.645Z" fill="white"/>
+<path d="M34.8142 99.9578C32.2704 100.961 30.4612 100.1 29.3866 97.3741L1.79003 27.3838C0.715395 24.6583 1.44997 22.7941 3.99375 21.7911L24.3803 13.7529C37.4626 8.59462 47.3676 7.14556 54.0952 9.40565C60.8592 11.6514 65.7815 16.6808 68.8621 24.4939C71.9428 32.3069 71.7775 39.3423 68.3664 45.6C64.9916 51.8434 56.763 57.5442 43.6807 62.7025L35.3953 65.9693L45.3034 91.0983C46.378 93.8237 45.6434 95.688 43.0996 96.691L34.8142 99.9578ZM30.1511 52.669L38.382 49.4236C45.65 46.5579 50.2554 43.7553 52.1985 41.0157C54.1272 38.2397 54.3034 34.8531 52.7273 30.8557C51.1512 26.8583 48.7182 24.5215 45.4283 23.8451C42.1241 23.1325 36.838 24.209 29.57 27.0747L21.3391 30.3201L30.1511 52.669Z" fill="white"/>
+</svg>  
+   
           </a>
         </div>
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <Link href="/about" className="text-2xl font-bold">
+        Hello I am {" "}
+        <span className="text-blue-400">
+      <Typewriter words={["Prashant Varma", "Software Engineer", "Indie Hacker"]} loop={Infinity} cursor={false} typeSpeed={100} deleteSpeed={100} delaySpeed={1000} />
+      </span>
+      </Link>
       </div>
+
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
