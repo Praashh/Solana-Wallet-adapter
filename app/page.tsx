e5ecb18814113622406e4fd9d2428db6e5155138
Praash <99237795+Praashh@users.noreply.github.com>
2024-05-29 10:11:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
     </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 linkedin-companies"
             href="https://www.linkedin.com/directory/companies"
             target="_blank"
             rel="noopener nofollow ugc"
